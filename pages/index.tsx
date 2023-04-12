27c62d77946680312932460dd3829588a3580911
shkim <shkim@shkimui-MacBookPro.local>
2023-04-12 10:13:12 +0900

    feat: 새 환경 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h2 className="text-3xl font-bold text-red-600">new env.</h2>
+    </div>
+    
+  )
+
 }
